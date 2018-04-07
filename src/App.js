c1c48c87fc9f5e8a250703ad3c97304c28caef66
hpstream <915414300@qq.com>
2018-04-07 22:22:58 +0800

    更新部分简历代码

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import './lib/css/index.scss'
 import './App.css';
 
+//个人信息，求职意向，专业技能，我的文章，求职经历 自我评价
+const personInfo ={
+    title:['黄鹏','求职意向：前端开发','手机号：13155183095'],
+    info:{
+      '个人信息':[],
+
+    }
+}
 class App extends Component {
+  constructor(){
+      super();
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className="person-info">
+        <header className="title p4 flex justify-around">
+          { personInfo.title.map((item, i)=>{
+            return <div  key={i}>{item}</div>
+          })}
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <p className="App-intro clearfx">
+          
         </p>
       </div>
     );
